b0cfc264af5e5502211341292fdcdb6b6b95feaf
Julian <juliandemourgues@gmail.com>
2016-12-11 18:59:20 +0100

    animation fix 2

--- web/app/components/dropDown.js
+++ web/app/components/dropDown.js
@@ -2,6 +2,11 @@ define(["foundation"], function (f) {
 
   var ANIMATING = false;
   var FIXED = false;
+  var PREVIOUS = false;
+
+  setInterval(function () {
+    console.log(FIXED)
+  }, 200);
   var animateToggle = function () {
     ANIMATING = true;
     $("#dropdown").animate({
@@ -18,23 +23,28 @@ define(["foundation"], function (f) {
       animateToggle()
     }
   })
-  .click(function () {
+  .on("click dblclick",function () {
     var display = $("#dropdown").css("display");
     if (display === "block" && FIXED && !ANIMATING) {
+      PREVIOUS = true;
       animateToggle();
       FIXED = !FIXED;
     } else if (display === "block" && !FIXED && !ANIMATING) {
       FIXED = !FIXED;
-    } else if (display !== "block" && !FIXED && !ANIMATING) {
+    } else if (display !== "block" && !FIXED) {
+      FIXED = !FIXED;
       animateToggle();
+    } else {
       FIXED = !FIXED;
     }
 
   });
   $("#viewer").mouseenter(function () {
     var display = $("#dropdown").css("display");
-    if (display === "block" && !FIXED) {
-      animateToggle()
+    if(!FIXED && !PREVIOUS) {
+      if (display === "block") {
+        animateToggle()
+      }
     }
   })
   .click(function () {
